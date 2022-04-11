4583cdcb26189b56ed4426d404e75b84c2e2a45e
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-04-11 08:02:35 -0700

    Create raio_do_paralelo.m

---
+++ funcoes/raio_do_paralelo.m
@@ -0,0 +1,8 @@
+function raio = raio_do_paralelo(latitude)
+    %calculo de latitude decimal
+    latitudeDecimal = latitude(1) + latitude(2)/60 + latitude(3)/3600;
+    
+    R = 6371;
+    
+    raio = R * cosd(latitudeDecimal)
+    
