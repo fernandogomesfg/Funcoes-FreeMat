7769f8407b1f2662d81f30471a7a9a405f472c07
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-04-11 14:23:06 -0700

    Create rad_to_deg.m

---
+++ funcoes/rad_to_deg.m
@@ -0,0 +1,9 @@
+function rad_to_deg(rad)
+    clc
+    printf('Recebe um escalar (angulo em radianos e converte-o em graus decimais)\n')
+    printf('Por Fernando Gomes\n\n\n')
+    printf('Angulo em radianos recebido: %f\n' ,rad)
+    printf('Resultado (graus decimais): %f\n',rad * 180 / pi )
+    
+   
+    
