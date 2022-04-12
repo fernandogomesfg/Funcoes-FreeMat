f946bd28f670ecd2679d95de01fb1e1c147a9997
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-04-12 12:41:05 -0700

    Update convCoordeGeoToRect.m

--- funcoes/convCoordeGeoToRect.m
+++ funcoes/convCoordeGeoToRect.m
@@ -1,4 +1,4 @@
-function convCoordeGeoToRect (la, lo)
+function convCoordeGeoToRect (lat, lon)
     printf('Tendo coordenadas geográficas de um ponto, calcular as suas respectivas coordenadas cartesianas \n')
     printf('Recebe um vector seis elementos \n')
     printf('Formato do vetor: [latG latM latS longG longM longS]\n')
@@ -8,35 +8,32 @@ function convCoordeGeoToRect (la, lo)
         error('A funcao convCoordeGeoToRect deve receber exactamente 1 argumento')
     end
     
-    dimPonto1 = size(lat);
-    if(~((dimPonto1(1) == 1 & dimPonto1(2) == 6) | (dimPonto1(1) ==  6 & dimPonto1(2) == 1)))
-        error('A dimensao do vector no ponto deve de 1x6 ou 6x1')
-    end
+    
     
     %extracao das coordenadas dos pontos
-    lat = [lat(1) lat(2) lat(3)];
-    lon = [lon(4) lon(5) lon(6)];
+    latitude = [lat(1) lat(2) lat(3)];
+    longitude = [lon(4) lon(5) lon(6)];
     
     %coordenadas decimais
     %latitudes do ponto 
-    if((lat(1) < 0)  | (lat(2) < 0) | (lat(3) < 0))
-        latDecimal = -1 * ((abs(lat(1))) + (abs(lat(2))/60) + (abs(lat(3))/3600));
+    if((latitude(1) < 0)  | (latitude(2) < 0) | (latitude(3) < 0))
+        latDecimal = -1 * ((abs(latitude(1))) + (abs(latitude(2))/60) + (abs(latitude(3))/3600));
     else
-        lat1Decimal = ((lat(1)) + (lat(2)/60) + (lat(3)/3600));
+        lat1Decimal = ((latitude(1)) + (latitude(2)/60) + (latitude(3)/3600));
     end
     
     %longitude do ponto
-    if((lon(1) < 0)  | (lon(2) < 0) | (lon(3) < 0))
-        longDecimal = -1 * ((abs(lon(1))) + (abs(lon(2))/60) + (abs(lon(3))/3600));
+    if((longitude(1) < 0)  | (longitude(2) < 0) | (longitude(3) < 0))
+        longDecimal = -1 * ((abs(longitude(1))) + (abs(longitude(2))/60) + (abs(longitude(3))/3600));
     else
-        lonDecimal = ((lon(1)) + (lon(2)/60) + (lon(3)/3600));
+        lonDecimal = ((longitude(1)) + (longitude(2)/60) + (longitude(3)/3600));
     end
     
     r = 6371;
     
-    x = r * cosd(lat) * cosd(lon);
-    y = r * cosd(lat) * sind(lon);
-    z = r * sind(lat);
+    x = r * cosd(latDecimal) * cosd(lonDecimal);
+    y = r * cosd(latDecimal) * sind(lonDecimal);
+    z = r * sind(latDecimal);
     
     printf('X = %.3f km\n' ,x)
     printf('Y = %.3f km\n' ,y)
